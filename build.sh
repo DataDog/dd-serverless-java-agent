d4840538c8d9ec1d6f3ecd77efb1a6c425111234
Duncan Harvey <duncan.harvey@datadoghq.com>
2024-08-23 16:16:56 -0400

    create target directory

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 VERSION=$(awk -F'[<>]' '/<version>/{print $3; exit}' pom.xml)
 
+mkdir -p target
+
 docker buildx build --platform linux/amd64 \
     -t datadog/build-dd-serverless-java-agent:$VERSION \
     . --load
+
 dockerId=$(docker create datadog/build-dd-serverless-java-agent:$VERSION)
 docker cp $dockerId:/app/target/dd-serverless-azure-java-agent-$VERSION-jar-with-dependencies.jar ./target/dd-serverless-azure-java-agent-$VERSION-jar-with-dependencies.jar
